2539278ab37e6c5e83df26227c5cced66f13268f
totsnotabot <54818237+totsnotabot@users.noreply.github.com>
2020-04-26 18:14:34 -0400

    Icon Overhaul
    Completely redesigned all the icons and the favicon to be more consistent stylistically in anticipation for the site overhaul.

--- site2/pgs/wgscript.js
+++ site2/pgs/wgscript.js
@@ -1,1 +1,22 @@
 // JavaScript source code
+
+//Darkmode
+var dMQ = window.matchMedia("(prefers-color-scheme: dark)"); 
+var lMQ = window.matchMedia("(prefers-color-scheme: light)");
+if (dMQ.matches) {
+    themeSwitch('dark');
+}
+dMQ.addListener(function () {
+    themeSwitch('dark');
+});
+lMQ.addListener(function () {
+    themeSwitch('light');
+});
+
+function themeSwitch(theme) {
+    if (theme == 'dark') {
+
+    } else {
+
+    }
+};
