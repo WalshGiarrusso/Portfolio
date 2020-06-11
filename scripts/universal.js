26239b343f5db16c68644a73b6cf965037bab9a3
Walsh Giarrusso <walshgiarrusso@gmail.com>
2020-06-10 20:51:36 -0400

    basic styling

--- scripts/universal.js
+++ scripts/universal.js
@@ -1,4 +1,4 @@
 // JavaScript source code
 $(document).ready(function (){
-    alert("Site is currently under construction. Visit http://walshgiarrusso.com/old-site/home for previous version of site.");
+    //alert("Site is currently under construction. Visit http://walshgiarrusso.com/old-site/home for previous version of site.");
 });
