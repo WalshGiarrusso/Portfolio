fd0a402114d36fb5708ba24a1efab3ce3907cfbc
Walsh Giarrusso <walshgiarrusso@gmail.com>
2020-06-28 19:07:06 -0400

    nudge

--- scripts/universal.js
+++ scripts/universal.js
@@ -153,9 +153,9 @@ function changeColors(targets){
         "color": targets[1]
     });
     //border color
-    $('.BottomBorder, .TopBorder, input[type=color], input[type=radio]').css('border-color', targets[2])
+    $('.BottomBorder, .TopBorder, input[type=color], input[type=radio], input[type=checkbox]').css('border-color', targets[2])
     //button colors
-    $('.TrueButton.FormStyling, .FormStyling').css({
+    $('.TrueButton.FormStyling').css({
         "background-color": targets[1],
         "color": targets[0]
     });
