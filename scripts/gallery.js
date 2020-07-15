eab6f2fb6516736c553d414c48e07e148d8a4dbd
Walsh Giarrusso <walshgiarrusso@gmail.com>
2020-07-14 20:20:56 -0400

    JSON Progress Check  17

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -22,32 +22,32 @@ $("#hideFiltersButton").click(function(){
 //    filterOut(startDate, endDate, chosenType);
 //    return false;
 //});
-function filterOut(sD, eD, cT){
-    $('.Card').each(function(){
-        $(this).show();
-    });
-    if(cT !== "" && cT !== "all"){
-        $('.Card').each(function(){
-            if($(this).children('p').children('span').text().trim().toLowerCase() !== cT){
-                $(this).hide();
-            };
-        });
-    };
-    if(sD != "Invalid Date"){
-        $('.Card').each(function(){
-            if(new Date($(this).children('p').children('time').attr('datetime')) < sD){
-                $(this).hide();
-            };
-        });
-    };
-    if(eD != "Invalid Date"){
-        $('.Card').each(function(){
-            if(new Date($(this).children('p').children('time').attr('datetime')) > eD){
-                $(this).hide();
-            };
-        });
-    };
-};
+//function filterOut(sD, eD, cT){
+//    $('.Card').each(function(){
+//        $(this).show();
+//    });
+//    if(cT !== "" && cT !== "all"){
+//        $('.Card').each(function(){
+//            if($(this).children('p').children('span').text().trim().toLowerCase() !== cT){
+//                $(this).hide();
+//            };
+//        });
+//    };
+//    if(sD != "Invalid Date"){
+//        $('.Card').each(function(){
+//            if(new Date($(this).children('p').children('time').attr('datetime')) < sD){
+//                $(this).hide();
+//            };
+//        });
+//    };
+//    if(eD != "Invalid Date"){
+//        $('.Card').each(function(){
+//            if(new Date($(this).children('p').children('time').attr('datetime')) > eD){
+//                $(this).hide();
+//            };
+//        });
+//    };
+//};
 
 $('#submitGallerySearch').click(function(){
     handleSearch();
@@ -72,18 +72,32 @@ function handleSearch(){
 
 $(document).ready(function(){
     $.getJSON(baseUrl + "data/galleryCards.json", function(data){
-        mPageNum = Math.ceil((data.cards.length/6));
+        
         populateGallery(cPageNum, data.cards);
     });
 });
 
 function populateGallery(pageNumber, cardArr){
+    
     $('.Card').parent('li').show();
 
+    mPageNum = Math.ceil((cardArr.length/6));
+    
     for(i = ((pageNumber-1)*6), v = 0; v < 6; i++, v++){
         if(i >= cardArr.length){
-            $('.Card').parent('li').hide();
+
+            
+            
+            console.log('go');
+
+
+
+            $('.Card').parent('li').eq(v).hide();
         }else{
+
+            console.log('i:' +i +' '+cardArr[i].title+' '+cardArr[i].type);
+
+
             $('.CardTitle').eq(v).text(cardArr[i].title);
             $('.CardImage').eq(v).attr({
                 "src": cardArr[i].imgSrc,
@@ -105,29 +119,22 @@ $("#submitGalleryFilters").click(function(){
     var opArray;
     $.getJSON(baseUrl + "data/galleryCards.json", function(data){
         opArray = data.cards;
-        console.log('before: '+opArray);
+       
         $(data.cards).each(function(){
             if(chosenType !== "" && chosenType !== "all" && this.type.toLowerCase() !== chosenType){
                 opArray.splice($(opArray).index(this),1);
             };
         });
         
-
-
         $(opArray).each(function(){
             var cD = new Date(this.iDate);
             if((cD < startDate) || (cD > endDate)){
                 opArray.splice($(opArray).index(this),1);
             };
         });
-        console.log('after: '+opArray);
 
-       
+        populateGallery(1, opArray);
 
     });
-
-
-
-
     return false;
 });
